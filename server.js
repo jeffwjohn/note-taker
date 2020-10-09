94c1e803aa099b78116c699291e0e42df07c5ef5
Jeff Johnston <jeffwjohn@yahoo.com>
2020-10-08 20:54:46 -0500

    office hours

--- server.js
+++ server.js
@@ -49,6 +49,37 @@ function createNewNote(body, notesArray) {
     return body;
 }
 
+function findById(id, notesArray) {
+    const result = notesArray.filter(note => note.id === id)[0];
+    return result;
+    }
+
+function deleteNote(note, notesArray) {
+    console.log('ID:', note.id);
+    console.log('Before:', notesArray);
+    notesArray.splice(note.id, 1);
+    console.log('After:', notesArray)
+    fs.writeFileSync(
+        path.join(__dirname, './db/notes.json'),
+        JSON.stringify({
+            notes: notesArray
+        }, null, 2)
+    );
+    return result;
+    
+    
+     
+     
+    //  fs.writeFileSync(
+    //     path.join(__dirname, './db/notes.json'),
+    //     JSON.stringify({
+    //         notes: notesArray
+    //     }, null, 2)
+    // );
+    // // return finished code to post route for response
+    // return body;
+}
+
 function validateNote(note) {
     if (!note.title || typeof note.title !== 'string') {
         return false;
@@ -71,30 +102,13 @@ app.get('/api/notes', (req, res) => {
     if (req.query) {
         results = filterByQuery(req.query, results);
     }
-    console.log(req.query)
+   
     res.json(results);
 });
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
-});
-
-// app.post("/notes", (req, res) => {
-//     // set id based on what the next index of the array will be
-//     req.body.id = notes.length.toString();
-
-//     // if any data in req.body is incorrect, send 400 error back
-//     if (!validateNote(req.body)) {
-//       res.status(400).send("The note is not properly formatted.");
-//     } else {
-//       const note = createNewNote(req.body, notes);
-//       res.json(note);
-//     }
-//   });
-
 app.post('/api/notes', (req, res) => {
     // req.body is where our incoming content will be
-    console.log(req.body);
+    // console.log(req.body);
     // set id based on what the next index of the array will be
     req.body.id = notes.length.toString();
 
@@ -108,6 +122,32 @@ app.post('/api/notes', (req, res) => {
     }
 });
 
+app.delete("/api/notes/:id",  (req, res) => {
+   
+    const id = req.params.id;
+    console.log(id);
+    res.json(id);
+//   const index = notes.findIndex((note, index) => note.id == id);
+
+//   notes.splice(index, 1);
+
+//   return res.send();
+    
+//   const note = findById(req.params.id, notes);
+//   if (note) {
+//       console.log('Note:', note);
+//       const result = deleteNote(note, notes);
+//       res.json(result);
+//   } else {
+//     res.send(404);
+//   }
+  
+
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
