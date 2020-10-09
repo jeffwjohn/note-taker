443d6c3003cd752c1027c9e6155cc4c604ea8fef
Jeff Johnston <jeffwjohn@yahoo.com>
2020-10-09 13:16:19 -0500

    clean code

--- server.js
+++ server.js
@@ -40,13 +40,6 @@ function createNewNote(body, notesArray) {
     const note = body;
     notesArray.push(note);
 
-    // let id = maxId + 1;
-    // console.log('Id:', id);
-    // const index = notesArray.findIndex(n => n.id === id)
-    // console.log('Index:', index);
-    // if (index < maxId) {
-    // id = (maxId + 1).toString();
-    // } 
     fs.writeFileSync(
         path.join(__dirname, './db/notes.json'),
         JSON.stringify({
@@ -55,19 +48,12 @@ function createNewNote(body, notesArray) {
     );
     // return finished code to post route for response
     return body;
-}
-
-function findById(id, notesArray) {
-    const result = notesArray.filter(note => note.id === id)[0];
-    return result;
-}
+};
 
 function deleteNote(body, notesArray) {
     const note = body;
-    console.log('ID:', note.id);
-    console.log('Before:', notesArray);
     notesArray.splice(note.id, 1);
-    console.log('After:', notesArray)
+
     fs.writeFileSync(
         path.join(__dirname, './db/notes.json'),
         JSON.stringify({
@@ -75,19 +61,7 @@ function deleteNote(body, notesArray) {
         }, null, 2)
     );
     return notesArray;
-
-
-
-
-    //  fs.writeFileSync(
-    //     path.join(__dirname, './db/notes.json'),
-    //     JSON.stringify({
-    //         notes: notesArray
-    //     }, null, 2)
-    // );
-    // // return finished code to post route for response
-    // return body;
-}
+};
 
 function validateNote(note) {
     if (!note.title || typeof note.title !== 'string') {
@@ -97,7 +71,7 @@ function validateNote(note) {
         return false;
     }
     return true;
-}
+};
 //=======================================================
 // Routes
 //=======================================================
@@ -116,13 +90,11 @@ app.get('/api/notes', (req, res) => {
 });
 
 app.post('/api/notes', (req, res) => {
-    // req.body is where our incoming content will be
-    // console.log(req.body);
-    // set id based on what the next index of the array will be
+    
+    // set id based on what the highest id of the array will be
     const maxId = Math.max.apply(Math, notes.map(function (o) {
         return o.id;
     }));
-    console.log('MaxId', maxId);
 
     if (maxId > 0) {
         req.body.id = (maxId + 1).toString();
@@ -140,24 +112,12 @@ app.post('/api/notes', (req, res) => {
 });
 
 app.delete("/api/notes/:id", (req, res) => {
-    // console.log("Start", req);
-    // const id = req.params.id;
-    // const note = deleteNote(id, notes);
-    // res.json(note);
-
 
     const id = req.params.id;
-    const note = notes.filter(note => note.id === id)[0];
-
-    console.log(id);
-    console.log('Note', note)
-    console.log('Before:', notes);
-    console.log('Note.ID', note.id);
-
     const index = notes.findIndex(n => n.id === id);
-    console.log('Index:', index);
+ 
     notes.splice(index, 1);
-    console.log('After:', notes);
+  
     res.json(id);
     fs.writeFileSync(
         path.join(__dirname, './db/notes.json'),
@@ -165,22 +125,6 @@ app.delete("/api/notes/:id", (req, res) => {
             notes: notes
         }, null, 2)
     );
-    //   const index = notes.findIndex((note, index) => note.id == id);
-
-    //   notes.splice(index, 1);
-
-    //   return res.send();
-
-    //   const note = findById(req.params.id, notes);
-    //   if (note) {
-    //       console.log('Note:', note);
-    //       const result = deleteNote(note, notes);
-    //       res.json(result);
-    //   } else {
-    //     res.send(404);
-    //   }
-
-
 });
 
 app.get('*', (req, res) => {
@@ -188,5 +132,4 @@ app.get('*', (req, res) => {
 });
 
 app.listen(PORT, () => {
-    console.log(`API server now on port ${PORT}!`);
 });
